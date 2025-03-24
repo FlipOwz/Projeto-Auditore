715e7f429d12657145b6282089b58e8f87bac553
FlipOwz <felipegalvaosp@gmail.com>
2025-03-24 13:48:32 -0300

    Projeto iniciado

---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        beaufort: ['Beaufort for LOL', 'sans-serif'],
+      },
+      colors: {
+        darkblue: '#00000A',
+      },
+    },
+  },
+  plugins: [],
+};
